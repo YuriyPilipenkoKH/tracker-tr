c3bd0720d283744a34e6bf4e4d1ed4cedb630079
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-28 21:12:00 +0200

    buttons + modal

---
+++ src/components/button/ThemeChanger.tsx
@@ -0,0 +1,20 @@
+
+import { useThemeStore } from '../../store/useThemeStore'
+import {  LuLightbulb, LuMoonStar } from "react-icons/lu";
+import { FlatButton } from './Button';
+
+
+const ThemeChanger = () => {
+  const {theme, toggleTheme} = useThemeStore()
+  return (
+    <FlatButton
+      className="btn btn-ghost text-[var(--text-color)]"
+      onClick={toggleTheme}
+      aria-label="Toggle Theme"
+    >
+      {theme === 'light' ? <LuMoonStar size={25}/> : <LuLightbulb  size={25}/>}
+    </FlatButton>
+  )
+}
+
+export default ThemeChanger
